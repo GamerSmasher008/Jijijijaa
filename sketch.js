926f3dde395bb637f6dbcec7f6839ea3dfdc153f
GamerSmasher008 <101297945+GamerSmasher008@users.noreply.github.com>
2022-07-25 10:51:10 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+var blastImage;
+
+function preload() {
+  
+}
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
